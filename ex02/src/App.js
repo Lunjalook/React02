87ec06452f486b0469f363cb6f1d7599697d3a16
Lunjalook <azra.cosickic@gmail.com>
2022-05-26 11:04:15 +0200

    Adding exercise 02

---
+++ ex02/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import Table from "./Table";
+
+class App extends Component{
+    render() {
+        return(
+            <div clasName="container">
+                <Table />
+            </div>
+        )
+    }
+}
+
+export default App;
