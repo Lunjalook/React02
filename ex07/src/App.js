6907a72adee37e369d78f2ea2b84b4dc40fc5e2f
Lunjalook <azra.cosickic@gmail.com>
2022-05-26 12:03:16 +0200

    Adding exercise ex07

---
+++ ex07/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import Box from './Box'
+
+class App extends Component{
+    render() {
+        return(
+            <div clasName="container">
+                <Box />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ ex07/src/Box.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Box =()=> {
+   
+        return (
+            <div>This is a Box Component!</div>
+        )
+    }
+
+
+export default Box;
