1b6c0906656d9008fd3a161286d36a673581036e
Lunjalook <azra.cosickic@gmail.com>
2022-05-26 12:37:05 +0200

    Adding exercise 08

---
+++ ex08/src/TableHeader.js
@@ -0,0 +1,17 @@
+
+const TableHeader =()=>{
+
+    return (
+       
+<thead>
+    <tr>
+        <th>First name</th>
+        <th>Last name</th>
+    </tr>
+</thead>
+
+    )
+}
+
+
+export default TableHeader;
